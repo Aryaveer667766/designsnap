def9bb603ade4a53a8fa92e36a436bb1aebe2ae8
Aryaveer667766 <aryaveer0121@gmail.com>
2025-08-30 20:15:18 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,19 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getDatabase, ref as dbRef, push, set, onValue, remove } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
-import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+import { getDatabase, ref, push, set, onValue, remove } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAZ_c3EaDxSZ6RrNrwQjR_TauJHrPfk_lk",
   authDomain: "designsnap-f3309.firebaseapp.com",
   databaseURL: "https://designsnap-f3309-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "designsnap-f3309",
-  storageBucket: "designsnap-f3309.firebasestorage.app",
+  storageBucket: "designsnap-f3309.appspot.com",
   messagingSenderId: "741888249963",
   appId: "1:741888249963:web:9540c9f68d6f0d5da9a12e",
   measurementId: "G-YC8TDQF1MM"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getDatabase(app);
-const storage = getStorage(app);
 
-// Export modules
-export { db, storage, dbRef, sRef, push, set, onValue, remove, uploadBytesResumable, getDownloadURL, deleteObject };
+export { db, ref, push, set, onValue, remove };
